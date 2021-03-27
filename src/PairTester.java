d1636a80a693d52cb5cdc0e6feceb4b22c8dc22c
Kuddleman <edl1155@gmail.com>
2021-03-26 18:16:57 -0700

    build PairTester class

---
+++ src/PairTester.java
@@ -0,0 +1,8 @@
+public class PairTester {
+
+    public static void main(String[] args) {
+        Pair<Integer> numberPair1 = new Pair<>(3,4);
+        Pair<Integer> numberPair2 = new Pair<>(5,6);
+        Pair<String> wordPair1 = new Pair<>("apple", "banana");
+    }
+}
