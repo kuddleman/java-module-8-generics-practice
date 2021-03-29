68068244b98963632fbafb474b3d068b95090be7
Kuddleman <edl1155@gmail.com>
2021-03-29 16:07:48 -0700

    build minmax method

--- src/ArrayUtılıtıes.java
+++ src/ArrayUtılıtıes.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class ArrayUtılıtıes {
     //we're using Integer OBJECT, so to compare we need to use equals()
@@ -23,6 +26,56 @@ public class ArrayUtılıtıes {
                 }
             }
         }
+        return duplicates;
+    }
+
+    // find duplicates method as a generic method:
+   // <T> define generic type for the method
+    // ArrayList<T>   define return type
+
+    public static <T> ArrayList<T> findDuplicates(T[] array) {
+        ArrayList<T> duplicates = new ArrayList<T>();
+        for(int i = 0; i < array.length; i++) {
+            for(int j = i + 1; j < array.length; j++) {
+                if(array[i].equals(array[j]) && !duplicates.contains(array[i])) {
+                    duplicates.add(array[i]);
+                }
+            }
+        }
+        return duplicates;
+    }
+
+    public static <T> List<T> findDuplicatesEfficient(T[] array) {
+        ArrayList<T> duplicates = new ArrayList<T>();
+        // sets don't allow duplicates!
+        Set<T> duplicateSet = new HashSet<T>();
+        Set<T> arrayValuesSet = new HashSet<T>();
+
+        for(int i = 0; i < array.length; i++) {
+            T value = array[i];
+            if(!arrayValuesSet.add(value)) {
+                duplicateSet.add(value);
+            }
+        }
+        duplicates.addAll(duplicateSet);
+        return duplicates;
+    }
+
+
+    public static Pair<Integer> findMinMaxIntegers(Integer[] array) {
+        int min = array[0];
+        int max = array[0];
+
+        for(int i = 0; i < array.length; i++) {
+            if(min > array[i]) {
+                min = array[i];
+            }
+            if(max < array[i]) {
+                max = array[i];
+            }
+        }
+        Pair<Integer> minMax = new Pair<>(min, max);
+        return minMax;
     }
 
 }
