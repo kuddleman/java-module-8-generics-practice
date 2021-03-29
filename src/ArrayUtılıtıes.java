e2b69d122d3328733a095525108c826cf76e31c8
Kuddleman <edl1155@gmail.com>
2021-03-29 15:24:02 -0700

    add ArrayUtilities class

---
+++ src/ArrayUtılıtıes.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class ArrayUtılıtıes {
+    //we're using Integer OBJECT, so to compare we need to use equals()
+    public static ArrayList<Integer> findDuplicateIntegers(Integer[] numbers) {
+        ArrayList<Integer> duplicates = new ArrayList<>();
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = i + 1; j < numbers.length; j++) {
+                if(numbers[i].equals(numbers[j]) && !duplicates.contains(numbers[i])) {
+                    duplicates.add(numbers[i]);
+                }
+            }
+        }
+        return duplicates;
+    }
+
+    public static ArrayList<String>
+
+}
