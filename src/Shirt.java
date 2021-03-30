17e8ca3399364c02cdc215ec4c594203de8603f3
Kuddleman <edl1155@gmail.com>
2021-03-30 14:41:44 -0700

    add toString method to Color

--- src/Shirt.java
+++ src/Shirt.java
@@ -1,2 +1,18 @@
-public class Shirt {
+import java.awt.*;
+
+public class Shirt extends Clothing {
+
+    private Color color;
+
+    public Shirt(String name, Size size, Color color) {
+        super(name, size);
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\n Color: R" + color.getRed() +
+                " G" + color.getGreen() + " B" + color.getBlue();
+    }
+
 }
