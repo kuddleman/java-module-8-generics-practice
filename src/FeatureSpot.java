f782c10509513174165a0dcd2982a3cd5d85440f
Kuddleman <edl1155@gmail.com>
2021-03-30 14:59:31 -0700

    complete FeaturedItem toString method

---
+++ src/FeatureSpot.java
@@ -0,0 +1,55 @@
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+public class FeatureSpot<T> {
+
+    private T item;
+    private LocalTime start, end;
+    private int featuredItemCount;
+
+    public FeatureSpot() {
+        this.featuredItemCount = 0;
+        this.item = null;
+    }
+
+    public void featureItem(T item, int numberOfMinutes) {
+        featuredItemCount ++;
+        this.item = item;
+        this.start = LocalTime.now();
+        this.end = this.start.plusMinutes(numberOfMinutes);
+    }
+
+    public boolean isAnythingFeatured() {
+        LocalTime now = LocalTime.now();
+        if (start.isBefore(now) && now.isBefore(end)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public T getItem() {
+        return item;
+    }
+
+    public LocalTime getStart() {
+        return start;
+    }
+
+    @Override
+    public String toString() {
+        if(!isAnythingFeatured()) {
+            return "There is no featured item at this time." +
+                    "\n\tThere have been " + featuredItemCount + " items featured.";
+        } else {
+            return "Featured Item!\n" + item.toString() +
+                    "\nThis item will be featured until " + end.truncatedTo(ChronoUnit.MINUTES) +
+                    "\nThere have been " + featuredItemCount + " items featured.\n";
+        }
+    }
+
+
+
+
+
+}
